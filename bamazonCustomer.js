222dac1c33488af0d8f8e1678f9a27148dfbad97
Anthony McPherson <aqm28202@gmail.com>
2019-10-11 15:28:48 -0400

    adding mysql connection

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,14 @@
+// Node modules required
+
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+
+// mySQL connection
+
+var connection = mysql.createConnection({
+	host: "localhost",
+	port: "3306",
+	user: "root",
+	password: "password",
+	database: "bamazon_db"
+});
