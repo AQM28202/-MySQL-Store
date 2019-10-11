283a368ccec0e6a356863c06598690728a197f77
Anthony McPherson <aqm28202@gmail.com>
2019-10-11 19:14:57 -0400

    logic is incomplete, updated qtys not displaying or registering

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,6 +14,7 @@ var connection = mysql.createConnection({
 });
 
 
+
 connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     console.log("Item    Product \t\tDepartment \tPrice\t Stock");
@@ -22,4 +23,53 @@ connection.query("SELECT * FROM products", function (err, res) {
         console.log(res[i].item_id + " \t" + res[i].product_name + "\t\t" + res[i].department_name + "\t" + res[i].price + " \t " + res[i].stock_quantity);
     }
     console.log("------------------------------------------------------------------");
+
+    inquirer.prompt([{
+		name: "product",
+		type: "input",
+		message: "What is the ID of the product that works best for you? [Changed your mind? Press Y]"
+	},
+	{
+		name: "qty",
+		type: "input",
+		message: "You will claim your write-off later, so how many of this product did you have in mind?"
+
+
+
+
+	}]).then(function (productObj) {
+		if (productObj.product.toUpperCase() == "Y") {
+			connection.end();
+		} else {
+			connection.query('SELECT * FROM products WHERE ?', { item_id: productObj.product }, function (err, res) {
+				if (err) throw err;
+				// console.log(res)
+				if (res[0].stock_quantity > productObj.qty) {
+
+					var cost = res[0].price * productObj.qty
+					console.log("-----------------------------------");
+					console.log("Your order is validated! \nThe total cost is $" + cost.toFixed(2) + "\nThank you for ordering")
+
+					var newQty = res[0].stock_quantity - productObj.qty
+
+					connection.query("UPDATE products SET ? WHERE ?", [{
+						stock_quantity: newQty
+					},
+					{
+						product_name: productObj.product
+					}],
+
+						function (err, res) {
+						});
+				}
+				else {
+					console.log("-----------------------------------");
+					console.log("Sorry, we do not have enough in stock. \nWe only have " + res[0].StockQuantity + " units of " + productObj.product + ". \nPlease retry your order. \nThank you!")
+				}
+			})
+		}
+	})
 });
+
+
+
